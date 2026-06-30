f84de82948625c561f01a3a112a449ca353730b4
MBarthe1UMBC <mbarthe1@umbc.edu>
2026-06-30 15:10:59 -0400

    added (I think incorrect and should be folded into the iteration function later) implementation of exeMemOp

--- finalProj.cpp
+++ finalProj.cpp
@@ -1,5 +1,28 @@
 #include "finalProj.h"
 
+public void exeMemOp(Instruction instructy){
+	switch (instructy.type) {
+		//floats take 6 cycles (2 EX stage)
+		case 1: //load float
+			if (instructy.cyclesSoFar == 6)
+				instructy.completed = true
+		case 2: //store float
+			if (instructy.cyclesSoFar == 6)
+				instructy.completed = true
+		case 3: //load immediate
+			if (instructy.cyclesSoFar == 5)
+				instructy.completed = true
+		//int takes 5 cycles
+		case 4: //load int
+			if (instructy.cyclesSoFar == 5)
+				instructy.completed = true
+		case 5: //store int
+			if (instructy.cyclesSoFar == 5)
+				instructy.completed = true
+			
+	}
+}
+
 int main{
 	
 	return 0
