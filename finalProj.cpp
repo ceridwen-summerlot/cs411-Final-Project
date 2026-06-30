e2b8d04425fe36ea396c5a6e9d5bcd2a12b29a5b
csummer2 <csummer2@linux3.gl.umbc.edu>
2026-06-30 14:26:49 -0400

    added enum for operations

--- finalProj.cpp
+++ finalProj.cpp
@@ -1,5 +1,23 @@
 #include "finalProj.h"
 
+enum Operations{
+	L.D,
+	S.D,
+	LI,
+	LW,
+	SW,
+	ADD,
+	ADDI,
+	ADD.D,
+	SUB.D,
+	SUB,
+	MUL.D,
+	DIV.D,
+	BEQ,
+	BNE,
+	J	
+};
+
 int main{
 	
 	return 0
