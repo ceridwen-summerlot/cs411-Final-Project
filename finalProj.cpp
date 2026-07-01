38d7bedac8a2837f4deca6c64fc834a04abe737a
csummer2 <csummer2@linux2.gl.umbc.edu>
2026-07-01 12:01:26 -0400

    added the other exe functions at the level of exeMemOp

--- finalProj.cpp
+++ finalProj.cpp
@@ -1,18 +1,18 @@
 #include "finalProj.h"
 
 enum Operations{
-	L.D,
-	S.D,
+	LD,
+	SD,
 	LI,
 	LW,
 	SW,
 	ADD,
 	ADDI,
-	ADD.D,
-	SUB.D,
+	ADDD,
+	SUBD,
 	SUB,
-	MUL.D,
-	DIV.D,
+	MULD,
+	DIVD,
 	BEQ,
 	BNE,
 	J	
@@ -21,25 +21,87 @@ enum Operations{
 public void exeMemOp(Instruction instructy){
 	switch (instructy.type) {
 		//floats take 6 cycles (2 EX stage)
-		case 1: //load float
-			if (instructy.cyclesSoFar == 6)
-				instructy.completed = true
-		case 2: //store float
-			if (instructy.cyclesSoFar == 6)
-				instructy.completed = true
-		case 3: //load immediate
-			if (instructy.cyclesSoFar == 5)
-				instructy.completed = true
+		case LD: //load float
+			if (instructy.cyclesSoFar == 6){
+				instructy.completed = true;
+			}
+		case SD: //store float
+			if (instructy.cyclesSoFar == 6){
+				instructy.completed = true;
+			}
+		case LI: //load immediate
+			if (instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
 		//int takes 5 cycles
-		case 4: //load int
-			if (instructy.cyclesSoFar == 5)
-				instructy.completed = true
-		case 5: //store int
-			if (instructy.cyclesSoFar == 5)
-				instructy.completed = true
+		case LW: //load int
+			if (instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
+		case SW: //store int
+			if (instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
 			
 	}
 }
+
+public void exeALUOp(Instruction instructy){
+	switch (instructy.type){
+		//integer add takes 1 cycle
+		case ADD:
+			if (instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
+	
+		case ADDI:
+			if (instructy.cyclesSoFar == 5){
+                                instructy.completed = true;
+                        }
+		//float add takes 2 cycles
+		case ADDD:
+			if (instructy.cyclesSoFar == 6){
+                                instructy.completed = true;
+                        }
+		case SUBD:
+			if (instructy.cyclesSoFar == 6){
+                                instructy.completed = true;
+                        }
+		//integer sub takes 1 cycles
+		case SUB:
+			if (instructy.cyclesSoFar == 5){
+                                instructy.completed = true;
+                        }
+		//float mult takes 10 cycles
+		case MULD:
+			if (instructy.cyclesSoFar == 14){
+                                instructy.completed = true;
+                        }
+		//float division takes 40 cycles
+		case DIVD:
+			if (instructy.cyclesSoFar == 44){
+                                instructy.completed = true;
+                        }
+	}
+}
+//this is super wrong for the actually execution and here as a placeholder
+public void exeBranchOp(Instruction instructy){
+	switch (instructy.type){
+		case BEQ:
+			if(instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
+		case BNE: 
+			if(instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
+		case J:
+			if(instructy.cyclesSoFar == 5){
+				instructy.completed = true;
+			}
+	}
+}
+
 
 int main{
 	
