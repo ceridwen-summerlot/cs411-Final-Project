7bd1a140217c60f556e8db8662e8da09befdc4ac
csummer2 <csummer2@linux2.gl.umbc.edu>
2026-07-01 13:30:00 -0400

    added read instruction function

--- finalProj.cpp
+++ finalProj.cpp
@@ -102,7 +102,92 @@ public void exeBranchOp(Instruction instructy){
 	}
 }
 
+public Instruction readNextInstruction(ifstream inputFile){
+	Instruction instruction;
+	string line;
+	string words[4];
+	int count;
+       	if(getline(inputFile, line){
+		istringstream iss(line);
+		string word;
 
+		while(iss >> word){
+			words[count] = word;
+			count++;
+		}
+	}
+	if(words[0] == "L.D"){
+		instruction.type = LD;
+		instruction.dest = words[1];
+		instruction.src1 = words[2];
+	}else if(words[0] == "S.D"){
+		instruction.type = SD;
+		instruction.dest = words[2];
+		instruction.src1 = words[1];
+	}else if(words[0] == "LI"){
+		instruction.type = LI;
+		instruction.dest = words[1];
+		instruction.src1 = words[2];
+	}else if(words[0] == "LW"){
+		instruction.type = LW;
+		instruction.dest = words[1];
+		instruction.src1 = words[2];
+	}else if(words[0] == "SW"){
+		instruction.type = SW;
+		instruction.dest = words[2];
+		instruction.src1 = words[1];
+	}else if(words[0] == "ADD"){
+		instruction.type = ADD;
+		instruction.dest = words[1];
+		instruction.src1 = words[2];
+		instruction.src2 = words[3];
+	}else if(words[0] == "ADDI"){
+		instruction.type = ADDI;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "ADD.D"){
+		instruction.type = ADDD;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "SUB.D"){
+		instruction.type = SUBD;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "SUB"){
+		instruction.type = SUB;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "MUL.D"){
+		instruction.type = MULD;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "DIV.D"){
+		instruction.type = DIVD;
+                instruction.dest = words[1];
+                instruction.src1 = words[2];
+                instruction.src2 = words[3];
+	}else if(words[0] == "BEQ"){
+		instruction.type = BEQ;
+		instruction.dest = words[3];
+		instruction.src1 = words[1];
+		instruction.src2 = words[2];
+	}else if(words[0] == "BNE"){
+		instruction.type = BNE;
+		instruction.dest = words[3];
+		instruction.src1 = words[1];
+		instruction.src2 = words[2];
+	}else if(words[0] == "J"){
+		instruction.type = J;
+		instruction.dest = words[1];
+	}
+	return instruction;
+
+}
 int main{
 	
 	return 0
